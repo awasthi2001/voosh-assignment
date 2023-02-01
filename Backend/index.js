851cb3fcd23ccf28b64274852f307034fb2518d3
awasthi2001 <akshanshawasthi013@gmail.com>
2023-02-01 18:27:16 +0530

    server

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,25 @@
-import express from 'express'
+import express from "express";
+import { Connection } from "./config/db.connect.js";
+import { authenticate } from "./Middleware/Authenticate.js";
+import { userRouter } from "./routes/users.routes.js";
+import cors from "cors";
+import { OrderRouter } from "./routes/order.routes.js";
+const app = express();
+const PORT = 8080;
+app.use(express.json());
+app.use(cors());
+
+app.use("/user", userRouter);
+app.use(authenticate);
+app.use("/order", OrderRouter);
+
+app.listen(PORT, () => {
+  Connection()
+    .then((res) => {
+      console.log(res);
+      console.log("Listening on", PORT);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
